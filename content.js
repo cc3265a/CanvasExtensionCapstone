395cddfa009b7bee8ada0134160494f29d784b50
cc3265a <cc3265a@american.edu>
2025-10-02 17:08:50 -0400

    ported code from prev repo

---
+++ content.js
@@ -0,0 +1,28 @@
+console.log("this is a web page");
+
+let hostNameVar = window.location.hostname;
+// console.log(hostNameVar);
+let dotLoc = hostNameVar.indexOf(".");
+// console.log(dotLoc);
+let newName = hostNameVar.slice(dotLoc+1);
+console.log(newName + " is the host name!");
+
+if (newName == "instructure.com"){
+  console.log("this is canvas")
+}
+
+
+
+// addEventListener("DOMContentLoaded", (event) => { 
+//   let textDiv = document.getElementsByClassName("input-block-level");
+//   textDiv.innerHTML = "<p style='background-color: red'>";
+//   console.log(textDiv.innerText);
+// })
+
+addEventListener("click", findBox);
+
+function findBox(e){
+  let textDiv = document.getElementsByName("tox-edit-area");
+  // textDiv.innerHTML = "<p style='background-color: red'>";
+  console.log(textDiv.item);
+}
