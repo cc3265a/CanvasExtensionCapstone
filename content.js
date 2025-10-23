f50dfb79d62de65a139d9659c36d4433ff3b196e
JuliaDoesThings <jj3579a@american.edu>
2025-10-23 15:18:13 -0400

    updated with code from Rin

--- content.js
+++ content.js
@@ -1,41 +1,38 @@
-console.log("this is a web page");
+// console.log("this is a web page");
 
 let hostNameVar = window.location.hostname;
 // console.log(hostNameVar);
 let dotLoc = hostNameVar.indexOf(".");
 // console.log(dotLoc);
 let newName = hostNameVar.slice(dotLoc+1);
-console.log(newName + " is the host name!");
+// console.log(newName + " is the host name!");
 
 if (newName == "instructure.com"){
-  console.log("this is canvas")
+  // console.log("this is canvas")
 }
 
-
-
-// addEventListener("DOMContentLoaded", (event) => { 
-//   let textDiv = document.getElementsByClassName("input-block-level");
-//   textDiv.innerHTML = "<p style='background-color: red'>";
-//   console.log(textDiv.innerText);
-// })
 
 addEventListener("click", findBox);
 
 function findBox(e){
-  let textDiv = document.querySelector("#tinymce");
-  // textDiv.innerHTML = "<p style='background-color: red'>";
-  console.log(textDiv);
+  let iframeObj = document.getElementById("textentry_text_ifr");
+  // console.log("iframe is" + iframeObj);
+
+  let iframeObj2 = document.getElementById("textentry_text_ifr").contentWindow;
+  // console.log("iframe2 is" + iframeObj2);
+
+  let pDiv = iframeObj2.document.querySelector("p");
+  // console.log(pDiv);
+
+  let innerHTMLObj = pDiv.innerHTML;
+  console.log(innerHTMLObj); 
+
+  // pDiv.innerText = "HELP ME";
+  // pDiv.innerHTML = <pre>  hello</pre>;
+
+  let tabObj= '&emsp;h';
+  pDiv.innerHTML = innerHTMLObj + tabObj;
+
+  let myItem = iframeObj2.document.querySelector("#tinymce");
+  // console.log(myItem.activeEditor.selection);
 }
-
-
-/* NOTES FROM QUIZ:
-Noteable div classes
-- class="form-control textarea-question-holder"
-- class="tox tox-tinymce tox-tinymce--toolbar-sticky-off"
-
-- class="tox-editor-container" (style editor BUTTONS HERE)
-
-
-- class = "tox-sidebar-wrap"
-  - class="tox-edit-area active""
-*/
