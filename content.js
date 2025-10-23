f5b25bcde443fbc532c37fa9d0167241b3f41093
JuliaDoesThings <jj3579a@american.edu>
2025-10-23 17:07:12 -0400

    added prototype button

--- content.js
+++ content.js
@@ -36,3 +36,40 @@ function findBox(e){
   let myItem = iframeObj2.document.querySelector("#tinymce");
   // console.log(myItem.activeEditor.selection);
 }
+
+
+//<button ="Italic" >
+function setUpButton() 
+{
+  let questionToolbars = document.querySelectorAll(tox-toolbar__primary);
+
+  let tabButton = document.createElement(button);
+  tabButton.classList.add("tox-tbtn");
+  tabButton.setAttribute("aria-label", "Tab");
+  tabButton.setAttribute("title", "Tab");
+  tabButton.setAttribute("type","button");
+  tabButton.setAttribute("tabindex", "-1");
+  tabButton.setAttribute("aria-disabled", "false");
+  tabButton.setAttribute("aria-pressed", "false");
+
+  let iconWrapper = document.createElement("span");
+  iconWrapper.classList.add("tox-icon tox-tbtn__icon-wrap");
+
+  //<svg viewBox="" xmlns="http://www.w3.org/2000/svg" focusable="false">
+  let iconSVG = document.createElement("svg");
+  iconSVG.setAttribute("viewBox", "0 0 1920 1920");
+  iconSVG.setAttribute("focusable", "false");
+  iconSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+  //path is the icon for the image (sample triangle from w3: M150 5 L75 200 L225 200 Z)
+  let iconImage = document.createElement("path");
+  iconImage.setAttribute("d", "M150 5 L75 200 L225 200 Z");
+  iconImage.setAttribute("fill-rule", "evenodd");
+
+  iconSVG.appendChild(iconImage);
+  iconWrapper.appendChild(iconSVG);
+  tabButton.appendChild(iconWrapper);
+
+}
+
+addEventListener("onclick", findbox());
