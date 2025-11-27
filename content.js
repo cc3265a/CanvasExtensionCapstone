bbe3c5eb52af72d0d2664935225140c1e390ba62
JuliaDoesThings <jj3579a@american.edu>
2025-11-27 13:32:15 -0600

    finished loading process, added button test functionality

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
+var toolboxCount = 0;
+const WebPageType = findType();
 
 window.onload = function() {
     console.log("onload ran");
@@ -6,28 +8,25 @@ window.onload = function() {
 
 window.onscroll = function() 
 {
-    findToolbars("Quiz")
+    if (WebPageType == "Quiz") 
+    {
+        if (countToolbars() != toolboxCount) 
+        {
+            processToolBars(WebPageType)
+        }
+    }
 }
-
-const WebPageType = findType();
-
-if (WebPageType != "none") 
-{
-    findToolbars(WebPageType);
-}
-   
-//findType();
 
 function findType() 
 {
     let pageType = "";
     const bodyObjects = document.querySelectorAll('body');
     bodyElement = bodyObjects[0];
-    console.log(bodyElement);
+    //console.log(bodyElement);
 
     const classString = bodyElement.className
-    console.log(classString);
-    console.log(typeof classString);
+    //console.log(classString);
+    //console.log(typeof classString);
 
     if (classString.includes("with"))
     {
@@ -56,11 +55,26 @@ function findType()
     return pageType;
 }
 
-function findToolbars(pageType) 
+function countToolbars() 
 {
-    let toolbars = []
+    let toolbars = [];
+    let allBars =  document.querySelectorAll(".tox-toolbar__group");     
+    //console.log("all bars: " + allBars.length)
+    for (const bar of allBars) 
+    {
+        if (bar.title == "Formatting") 
+        {
+            toolbars.push(bar);
+        }
+    }
+    return toolbars.length
+}
+
+function processToolBars(pageType) 
+{
+    let toolbars = [];
     console.log("finding tool bars");
-    console.log(pageType);
+    //console.log(pageType);
     if (pageType == "Quiz") 
     {
         let allBars =  document.querySelectorAll(".tox-toolbar__group");     
@@ -73,8 +87,8 @@ function findToolbars(pageType)
             }
         }
 
-        console.log(toolbars);
-        console.log("length " + toolbars.length)
+        //console.log(toolbars);
+        //console.log("length " + toolbars.length)
     }
     for (const bar of toolbars)     
     {
@@ -84,12 +98,13 @@ function findToolbars(pageType)
             addButton(bar)
         }
     }
+    toolboxCount = toolbars.length
 }
 
 function checkForButton(toolbar) 
 {
     var hasButton = toolbar.querySelector("#addedButton") != null;
-    console.log(toolbar + " status = " + hasButton)
+    //console.log(toolbar + " status = " + hasButton)
     return hasButton
 }
 
@@ -104,12 +119,13 @@ function addButton(toolbar)
     tabButton.tabIndex = "-1";
     tabButton.ariaDisabled = false;
     tabButton.ariaPressed = false;
-    //tabButton.addEventListener("click", printTest(event));
+    tabButton.addEventListener("click", printTest);
     toolbar.appendChild(tabButton);
 }
 
-function printTest(event) 
+function printTest(e) 
 {
-    event.preventDefault()
-    console.log("test button clicked")
+    e.preventDefault();
+    console.log("test button clicked");
+    return false;
 }
