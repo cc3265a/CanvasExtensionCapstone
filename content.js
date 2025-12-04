87e83bc4ed01c7488c0bafd798fa818fc4c1909a
cc3265a <cc3265a@american.edu>
2025-12-04 16:09:15 -0500

    e

--- content.js
+++ content.js
@@ -1,32 +1,36 @@
 
 
+console.log("here");
+var script = document.createElement('script');
+script.type = 'text/javascript';
 
-let hostNameVar = window.location.hostname;
-// console.log(hostNameVar);
-let dotLoc = hostNameVar.indexOf(".");
-// console.log(dotLoc);
-let newName = hostNameVar.slice(dotLoc+1);
-// console.log(newName + " is the host name!");
+script.src = ""; //'https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js';
+document.body.appendChild(script);
 
-if (newName == "instructure.com"){
-  // console.log("this is canvas")
-}
+const inputElement = document.getElementById("tinymce");
 
+addEventListener("click", add_tab);
 
-addEventListener("click", findBox);
+function add_tab(e){
 
-function findBox(e){
+  // console.log(tinymce.activeEditor.selection);
+
+  console.log("here");
+  // console.log(inputElement.selectionStart);
+
+  //grab the location of the textbox and what is currently in it
   let iframeObj = document.getElementById("textentry_text_ifr").contentWindow;
-
   let bodyDiv = iframeObj.document.querySelector("body");
-
   let innerHTMLObj = bodyDiv.innerHTML;
 
+  //the tab object to be inserted at will
   let tabObj= '&nbsp;';
 
+  //slice text into seperate paragraphs
   let pArray = innerHTMLObj.split("<p>");
   console.log(pArray);
 
+  //remove broken slices
   for (i = 0; i < pArray.length; i++){
     if (pArray[i] == ""){
       pArray.splice(i, 1);
@@ -34,20 +38,36 @@ function findBox(e){
   }
   console.log(pArray);
 
+  // get first paragraph
   let firstPArrObj = pArray[0];
   console.log("first pArrObj = " + firstPArrObj);
 
-  let newP = `<p>` + tabObj + firstPArrObj;
-  console.log("the newP is = " + newP);
+  let myNum = 1;
+  let newHTML;
+  let newP;
 
-  let newHTML = newP;
+  if (myNum === 0){
+    // add tab to first paragraph
+    newP = `<p>` + tabObj + firstPArrObj;
+    console.log("myNum == 0");
+  }
+  else{
+    newP = tabObj + firstPArrObj;
+    console.log("my num != 0");
+  }
+  newHTML = newP;
+  console.log("this newHTML is " + newHTML);
+
   for (let i = 1; i < pArray.length; i++){
-    newHTML = newHTML + `<p>&nbsp` + pArray[i];
+    if (i === myNum){
+      newHTML = newHTML + `<p>&nbsp` + pArray[i];
+    }
+    else{
+      newHTML = newHTML + pArray[i];
+    }
     console.log("i = " + pArray[i]);
   }
-  // newHTML = newHTML + newP;
-
-
+  
   bodyDiv.innerHTML = newHTML;
   console.log("newHTML = " + newHTML);
 
